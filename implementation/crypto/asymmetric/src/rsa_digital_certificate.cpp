e5e43f1db63b660b0e42ffd96312eea40975516d
Heo Jaewoong <wkdgjwodnd@naver.com>
2025-05-15 08:44:30 +0000

    make multicast json file

--- implementation/crypto/asymmetric/src/rsa_digital_certificate.cpp
+++ implementation/crypto/asymmetric/src/rsa_digital_certificate.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 // Copyright (C) 2019 Marco Iorio (Politecnico di Torino)
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
